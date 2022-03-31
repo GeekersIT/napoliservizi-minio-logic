8aeae893fab2a9d2611ece692b64a3fc677aa9a5
SAMaisto <salvatore.maisto@geekers.it>
2022-03-31 14:55:36 +0200

    fix env name

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ const config = {
         url: "https://" + process.env.PUBLIC_URI + "/auth",
         username: process.env.ADMIN_USER,
         password: process.env.ADMIN_PWD,
-        realm: process.env.KEYCLOAK_REALM,
+        realm: process.env.NAMESPACE,
     },
     db: {
-        url: "hasura."+process.env.KEYCLOAK_REALM+"/api/v1/graphql",
+        url: "hasura."+process.env.NAMESPACE+"/api/v1/graphql",
     },
     minio: {
-        url: "minio."+process.env.KEYCLOAK_REALM+":9000",
+        url: "minio."+process.env.NAMESPACE+":9000",
         accessKey: process.env.MINIO_ACCESS,
         secretKey: process.env.MINIO_SECRET
     }
